47e0ff858953db23369965bf6a1f5ed5be784116
Thiago Sanches <thiago.sanches0703@gmail.com>
2023-03-16 16:17:30 -0300

    adding rules on create user

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -6,11 +6,11 @@ import {
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateUserDto } from 'src/types/dtos/create-user.dto';
 import { UpdateUserDto } from 'src/types/dtos/update-user.dto';
-import { User } from '../../types/entities/user.entity'
-import { Repository } from 'typeorm';
-import { Operations } from './user.operations';
-import { UserAssembler } from './assembler/userAssembler';
 import { UserResponseDto } from 'src/types/dtos/user.response.dto';
+import { Repository } from 'typeorm';
+import { User } from '../../types/entities/user.entity';
+import { UserAssembler } from './assembler/userAssembler';
+import { Operations } from './user.operations';
 
 @Injectable()
 export class UserService implements Operations {
@@ -19,18 +19,22 @@ export class UserService implements Operations {
     private userRepository: Repository<User>,
   ) {}
   async createUser(data: CreateUserDto): Promise<UserResponseDto> {
+    const userAlredyExist = await this.getUserByEmail(data.email);
+    if (userAlredyExist) {
+      throw new InternalServerErrorException('User already exists');
+    }
     const user = this.userRepository.create(data);
     const userSaved = await this.userRepository.save(user);
     if (!userSaved) {
       throw new InternalServerErrorException('User was not created');
     }
-    const userDto = UserAssembler.assembleCreateUserToDto(userSaved)
+    const userDto = UserAssembler.assembleCreateUserToDto(userSaved);
     return userDto;
   }
 
   async getUsers(): Promise<UserResponseDto[]> {
     const users = await this.userRepository.find();
-    const usersDto = UserAssembler.assembleUsersToDto(users)
+    const usersDto = UserAssembler.assembleUsersToDto(users);
     return usersDto;
   }
 
